5c45d1640bb0e79d541ac26bccc99f1730be923b
oooozzzzz <lobovdima27@gmail.com>
2024-08-13 14:28:29 +0300

    test new layout

--- components/RecordItem.jsx
+++ components/RecordItem.jsx
@@ -12,21 +12,26 @@ function RecordItem({
 	createdAt,
 }) {
 	return (
-		<div className="w-full bg-gray-500/10 text-xl font-extralight my-3 p-2 flex justify-between items-center border-gray-400/70 rounded-xl border">
-			<span className="font-light pl-3">{amount}</span>
-			{categoryLabel ? (
-				<span className="font-light text-base">{categoryLabel}</span>
-			) : (
-				<span className="italic font-">{comment}</span>
-			)}
-			{createdAt ? (
-				<span className="font-light text-base">{createdAt}</span>
-			) : null}
-			<DeleteRecord
-				deleteRecord={deleteRecord}
-				recordId={recordId}
-				categoryId={categoryId}
-			/>
+		<div className="w-full bg-gray-500/10 text-xl font-extralight my-3 p-2 flex flex-col items-center border-gray-400/70 rounded-xl border">
+			<div className="flex justify-between items-center w-full">
+				<span className="font-light pl-3">{amount}</span>
+				{categoryLabel ? (
+					<span className="font-light text-base">{categoryLabel}</span>
+				) : (
+					<span className="italic font-">{comment}</span>
+				)}
+
+				<DeleteRecord
+					deleteRecord={deleteRecord}
+					recordId={recordId}
+					categoryId={categoryId}
+				/>
+			</div>
+			<div>
+				{createdAt ? (
+					<span className="font-light text-base">{createdAt}</span>
+				) : null}
+			</div>
 		</div>
 	);
 }
